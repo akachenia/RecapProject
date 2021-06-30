3957b49b6aece1d6f00d86895dd5b3bfffbc01b7
akachenia <akachenia@gmail.com>
2021-06-30 16:48:06 -0700

    salary 120000

--- src/test/java/UserStory100.java
+++ src/test/java/UserStory100.java
@@ -2,5 +2,6 @@ public class UserStory100 {
     public static void main(String[] args) {
         System.out.println("I love java");
         System.out.println("Akachenia SDET");
+        System.out.println("Salary is 120000");
     }
 }
