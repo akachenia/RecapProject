e4b8dca8f4454ffb4046036e16534e8a3ef03c3b
akachenia <82975753+akachenia@users.noreply.github.com>
2021-06-30 18:27:55 -0700

    US102 has been completed (#2)
    combined all 3 commits by squashing it

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,7 @@
+public class UserStory102 {
+    //lots of code has been written here
+    // ready to push this branch to remote
+    // and open pull request so i can start collaborating
+    // bla bla bla
+    // more changes added as requested
+}
