c98cb8aa39ec4491e58e548c6b7c25ea6123868b
akachenia <akachenia@gmail.com>
2021-06-30 18:18:18 -0700

    User Story 102 has finished

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,6 @@
+public class UserStory102 {
+    //lots of code has been written here
+    // ready to push this branch to remote
+    // and open pull request so i can start collaborating
+    // bla bla bla
+}
